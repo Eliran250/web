bef4e19a8b1bbcaf3052f94b8120acf9b60fd2a0
Eliran Giladi <elirangiladi250@gmail.com>
2025-06-29 18:34:33 +0300

    home

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,17 @@
-import './App.css'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import './style/style.scss'
+import Home from './components/home/Home'
 
 function App() {
 
   return (
-    <>
-    </>
+      <div className='app-container'>
+        <BrowserRouter>
+          <Routes>
+            <Route path='/' element={<Home />} />
+          </Routes>
+        </BrowserRouter>
+      </div>
   )
 }
 
---
+++ src/components/home/Home.tsx
@@ -0,0 +1,12 @@
+import '../home/homeStyle.scss'
+const Home = () => {
+  return (
+    <>
+      <div className="home-container">
+        <h1 className='title'>EG chat</h1>
+      </div>
+    </>
+  )
+}
+
+export default Home
