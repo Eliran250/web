8a4f8f8b63502eaaea11e3d13fd93210f45939b5
Eliran Giladi <elirangiladi250@gmail.com>
2025-10-21 20:48:06 +0300

    ipops

--- client/src/components/popup/employeesPopup/EmployeesPopup.tsx
+++ client/src/components/popup/employeesPopup/EmployeesPopup.tsx
@@ -1,11 +1,20 @@
 import { GiCrossedAxes } from "react-icons/gi"
 import './employeesStyle.scss'
+import type { User } from "../../../types/interface/employees"
 
-const EmployeesPopup = ({ selectedUser, openPopup,setOpenPopup }: { selectedUser: any, openPopup: any,setOpenPopup: any }) => {
+interface IProps {
+    openPopup: boolean,
+    selectedUser: User | null,
+    setOpenPopup: (openPopup: boolean) => void,
+}
+
+const EmployeesPopup = ({ selectedUser, openPopup, setOpenPopup }: IProps) => {
 
     return (
         <dialog open={openPopup}>
+            {selectedUser &&
                 <div>
+
                     <GiCrossedAxes size={20} className='exist-button' onClick={() => setOpenPopup(false)} />
                     <p>ID: {selectedUser.id}</p>
                     <p>Maiden Name: {selectedUser.maidenName}</p>
@@ -20,29 +29,32 @@ const EmployeesPopup = ({ selectedUser, openPopup,setOpenPopup }: { selectedUser
                     <p>Height: {selectedUser.height} cm</p>
                     <p>Weight: {selectedUser.weight} kg</p>
                     <p>Eye Color: {selectedUser.eyeColor}</p>
-                    <p>Hair Color: {selectedUser.hair.color}</p>
-                    <p>Hair Type: {selectedUser.hair.type}</p>
+                    <p>Hair Color: {selectedUser.hair?.color}</p>
+                    <p>Hair Type: {selectedUser.hair?.type}</p>
                     <p>IP: {selectedUser.ip}</p>
-                    <p>Address: {selectedUser.address.address}, {selectedUser.address.city}, {selectedUser.address.state} ({selectedUser.address.stateCode}) {selectedUser.address.postalCode}, {selectedUser.address.country}</p>
-                    <p>Coordinates: {selectedUser.address.coordinates.lat}, {selectedUser.address.coordinates.lng}</p>
+                    <p>Address: {selectedUser.address?.address}, {selectedUser.address?.city}, {selectedUser.address?.state} ({selectedUser.address?.stateCode}) {selectedUser.address?.postalCode}, {selectedUser.address?.country}</p>
+                    <p>Coordinates: {selectedUser.address?.coordinates?.lat}, {selectedUser.address?.coordinates?.lng}</p>
                     <p>MAC Address: {selectedUser.macAddress}</p>
                     <p>University: {selectedUser.university}</p>
-                    <p>Bank Card: {selectedUser.bank.cardNumber} ({selectedUser.bank.cardType})</p>
-                    <p>Card Expire: {selectedUser.bank.cardExpire}</p>
-                    <p>Currency: {selectedUser.bank.currency}</p>
-                    <p>IBAN: {selectedUser.bank.iban}</p>
-                    <p>Company: {selectedUser.company.name}</p>
-                    <p>Department: {selectedUser.company.department}</p>
-                    <p>Title: {selectedUser.company.title}</p>
-                    <p>Company Address: {selectedUser.company.address.address}, {selectedUser.company.address.city}, {selectedUser.company.address.state} ({selectedUser.company.address.stateCode}) {selectedUser.company.address.postalCode}, {selectedUser.company.address.country}</p>
+                    <p>Bank Card: {selectedUser.bank?.cardNumber} ({selectedUser.bank?.cardType})</p>
+                    <p>Card Expire: {selectedUser.bank?.cardExpire}</p>
+                    <p>Currency: {selectedUser.bank?.currency}</p>
+                    <p>IBAN: {selectedUser.bank?.iban}</p>
+                    <p>Company: {selectedUser.company?.name}</p>
+                    <p>Department: {selectedUser.company?.department}</p>
+                    <p>Title: {selectedUser.company?.title}</p>
+                    <p>Company Address: {selectedUser.company?.address?.address}, {selectedUser.company?.address?.city}, {selectedUser.company?.address?.state} ({selectedUser.company?.address?.stateCode}) {selectedUser.company?.address?.postalCode}, {selectedUser.company?.address?.country}</p>
                     <p>EIN: {selectedUser.ein}</p>
                     <p>SSN: {selectedUser.ssn}</p>
                     <p>User Agent: {selectedUser.userAgent}</p>
-                    <p>Crypto Coin: {selectedUser.crypto.coin}</p>
-                    <p>Wallet: {selectedUser.crypto.wallet}</p>
-                    <p>Network: {selectedUser.crypto.network}</p>
+                    <p>Crypto Coin: {selectedUser.crypto?.coin}</p>
+                    <p>Wallet: {selectedUser.crypto?.wallet}</p>
+                    <p>Network: {selectedUser.crypto?.network}</p>
                     <p>Role: {selectedUser.role}</p>
                 </div>
+
+            }
+
         </dialog>
     )
 }
--- client/src/types/interface/employees.ts
+++ client/src/types/interface/employees.ts
@@ -5,18 +5,81 @@ export interface Company {
 }
 
 export interface User {
-  id: number;
-  firstName: string;
-  lastName: string;
-  age: number;
+  id?: number;
+  firstName?: string;
+  lastName?: string;
+  maidenName?: string;
+  age?: number;
   gender?: string;
-  email: string;
+  email?: string;
   phone?: string;
-  image: string; // תמונת המשתמש
-  company: Company;
   username?: string;
+  password?: string;
   birthDate?: string;
+  bloodGroup?: string;
+  height?: number;
+  weight?: number;
+  eyeColor?: string;
+
+  hair?: {
+    color?: string;
+    type?: string;
+  };
+
+  image?: string; // תמונת המשתמש
+
+  ip?: string;
+  address?: {
+    address?: string;
+    city?: string;
+    state?: string;
+    stateCode?: string;
+    postalCode?: string;
+    country?: string;
+    coordinates?: {
+      lat?: number;
+      lng?: number;
+    };
+  };
+
+  macAddress?: string;
+  university?: string;
+
+  bank?: {
+    cardNumber?: string;
+    cardType?: string;
+    cardExpire?: string;
+    currency?: string;
+    iban?: string;
+  };
+
+  company?: {
+    name?: string;
+    department?: string;
+    title?: string;
+    address?: {
+      address?: string;
+      city?: string;
+      state?: string;
+      stateCode?: string;
+      postalCode?: string;
+      country?: string;
+    };
+  };
+
+  ein?: string;
+  ssn?: string;
+  userAgent?: string;
+
+  crypto?: {
+    coin?: string;
+    wallet?: string;
+    network?: string;
+  };
+
+  role?: string;
 }
+
 
 export interface UsersResponse {
   users: User[];
